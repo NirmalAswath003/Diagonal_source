a55cb8bf8e3e07c0bfc6fa6a460eca6da293dce2
NirmalAswath003 <nirmal.aswath@codoid.com>
2022-10-20 16:32:27 +0530

    Unit_test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,17 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-import configureMockStore from "redux-mock-store";
-
-const mockStore = configureMockStore(middlewares);
-console.log(mockStore, "mockstore");
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { update } from "../src/redux/data.js";
+import store from "./redux/Store";
+const data1 = require(`../src/Api/CONTENTLISTINGPAGE-PAGE${1}.json`);
+var data = data1.page["content-items"].content;
+describe("data redux state tests", () => {
+  it("Should initially set data to an empty object", () => {
+    const state = store.getState().data;
+    expect(state.value).toEqual([]);
+  });
+  it("Should be able to fetch the data and store the same", async () => {
+    const result = await store.dispatch(update(data));
+    const games = result.payload;
+    const state = store.getState().data;
+    const finaldata = state.value[0];
+    expect(finaldata).toBe(games);
+  });
 });
